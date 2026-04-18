31ce62c8d135ce45697420ee06838f81de3632b1
e_aizzaldin <e_aizzaldin@earthlink.iq>
2026-04-18 11:56:34 +0300

    Add: Api endpoints added

---
+++ Upload-large-files/Api.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// a post endpoint that accepts a file metadata (name, size,total chunks) and returns a unique upload id
+
+app.MapPost("/upload/init", (FileMetadata metadata)=> {
+    var uploadId = Guid.NewGuid();
+
+    return Results.Ok(new { uploadId });
+} );
+
+app.MapPost("/upload/chunk", (ChunkMetadata metadata) => {
+    return Results.Ok(new { message = "Chunk received" });
+});
+
+app.MapPost("/upload/finalize", (FinalizeMetadata metadata) => {
+    return Results.Ok(new { message = "Upload finalized" });
+});
